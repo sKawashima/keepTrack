b77c3031c10efe26c263448e16549b82fb3b2249
Shota Kawashima <bysk.333@gmail.com>
2018-04-09 09:39:05 +0900

    set manifest

---
+++ nuxt.config.js
@@ -0,0 +1,91 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'spa',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#d6c726' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [,
+    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
+    '@nuxtjs/bulma',
+    '@nuxtjs/pwa'
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      plugins: {
+        'postcss-cssnext': {
+          features: {
+            customProperties: false
+          }
+        }
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+
+  workbox: {
+    dev: true
+  },
+
+  manifest: {
+    name: 'Two Option Keep Track',
+    short_name: 'TOKT',
+    title: 'Two Option Keep Track',
+    'og:title': 'Two Option Keep Track',
+    description: '必要・不必要を記録するシンプルな家計簿',
+    'og:description': '必要・不必要を記録するシンプルな家計簿',
+    lang: 'ja',
+    theme_color: '#d6c726',
+    background_color: '#ffffff'
+  }
+}
