8a8ae4d73f040f16bb3e25a0fcd7a762b8fd2c7a
Shota Kawashima <bysk.333@gmail.com>
2018-04-09 10:31:36 +0900

    make nav

---
+++ components/Nav.vue
@@ -0,0 +1,32 @@
+<template lang='pug'>
+nav
+  nuxt-link(to='/').nav-contents.nav-title TOKT
+  p.nav-contents help
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='sass' scoped>
+nav
+  width: 100vw
+  height: 4rem
+  background-color: darken(#d6c726, 25%)
+  display: flex
+  justify-content: flex-start
+  align-items: center
+  .nav-contents
+    color: #fff
+    padding: 0 2rem
+    display: flex
+    justify-content: flex-start
+    align-items: center
+    height: 100%
+    &:hover
+      background-color: darken(#d6c726, 40%)
+  .nav-title
+    font-weight: bold
+    font-size: 1.5em
+</style>
